8df193ee2ca1772d272bf73bbd2c3716c22189f0
Daniel Leon Herrero <danielleon157@outlook.com>
2023-11-08 18:54:40 +0100

    Added plumber dependence for cath error in scss but not stop npm run dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require("gulp")
 const sass = require("gulp-sass")(require("sass"))
+const plumber = require('gulp-plumber')
 
 function css(cb) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
+        .pipe( plumber() )
         .pipe(sass())    // Compliar SCSS
         .pipe(dest('build/css')) // Alacenar el CSS
 
