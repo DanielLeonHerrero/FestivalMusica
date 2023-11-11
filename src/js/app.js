ec2b9c33dbad3dac65ee458d6365b732102ccd4c
Daniel Leon Herrero <danielleon157@outlook.com>
2023-11-11 13:52:05 +0100

    Galeria section finished

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,46 @@ function crearGaleria() {
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen Galeria">
         </picture>
         `
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture')
+        imagen.innerHTML = `
+        <picture>
+            <source srcset="build/img/grande/${id}.avif" type="image/avif">
+            <source srcset="build/img/grande/${id}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen Galeria">
+        </picture>
+        `;
+        // Crear overlay con imagen
+        const overlay = document.createElement('DIV');
+        overlay.appendChild(imagen)
+        overlay.classList.add('overlay')
+        overlay.onclick = function(){
+            overlay.remove();
+            const body = document.querySelector('body')
+            body.classList.remove('fijar-body')
+        }
+
+        // Crear boton para cerrar el Modal
+
+        const cerrarModal = document.createElement('P')
+        cerrarModal.textContent = 'X'
+        cerrarModal.classList.add('btn-cerrar')
+        cerrarModal.onclick = function() {
+            overlay.remove();
+            const body = document.querySelector('body')
+            body.classList.remove('fijar-body')
+        }
+        overlay.appendChild(cerrarModal)
+
+        // Añadirlo al HTML
+        const body = document.querySelector('body')
+        body.appendChild(overlay)
+        body.classList.add('fijar-body')
 }
